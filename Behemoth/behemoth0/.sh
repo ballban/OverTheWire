e1ab7d793d9b621267b54c611925d5c2e7987746
ballban <ballban@hotmail.com>
2025-01-19 01:48:57 +0900

    behemoth0,1

---
+++ Behemoth/behemoth0/.sh
@@ -0,0 +1,21 @@
+sftp -oPort=2221 behemoth0@narnia.labs.overthewire.org
+behemoth0
+get /behemoth/behemoth0
+exit
+
+sshpass -p 'behemoth0' ssh behemoth0@narnia.labs.overthewire.org -p 2221
+cd /behemoth
+
+gef ./behemoth0
+disassemble main
+b *main+123
+run
+
+# password eatmyshorts
+exit
+
+./behemoth0
+eatmyshorts
+
+cat /etc/behemoth_pass/behemoth1
+# password: 8YpAQCAuKf
