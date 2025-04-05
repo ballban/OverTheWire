b19da1273f5643283f505b762c0afa67bf1bd6c2
ballban_Jung <ballban@hotmail.com>
2025-04-06 01:31:36 +0900

    utumno1

--- Utumno/utumo1/preload.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-// gcc preload.c -o preload.so -fPIC -shared -ldl -m32
-int strncmp(const char *s1, const char *s2, size_t n)
-{
-    printf("Hello from 'strncmp' !");
-
-    return 0;
-}
